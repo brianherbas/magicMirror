2051c9cb38db878f2f1071573769e325c9b49298
Nicolás Rickert <nicorickert13@gmail.com>
2017-05-23 17:07:51 -0300

    comments added

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-import web
+import web #libreria para simular web-server
 import urlparse
 import json
-#import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO  #libreria para usar los pines de la raspberry
 
 def make_text(string):
     return string
 
+#lista de directorios y la clase que abre en cada uno
 urls = (
 		'/', 'index',
 		'/lz', 'luz'
 		)
+		
+#la carpeta a donde va a buscar el index.html		
 render = web.template.render('templates/')
 
 app = web.application(urls, globals())
@@ -18,6 +21,8 @@ app = web.application(urls, globals())
 #                web.form.Textbox('', class_='textfield', id='textfield'),
 #                )
 
+
+#control de los pines (17)
 class luz:
     def GET(self):
         #form = my_form()
@@ -40,6 +45,8 @@ class luz:
 	
 		return json.dumps({'msg': estado})
 		
+		
+#carga el render (/templates/index.html)
 class index:
     def GET(self):
         return render.index()		
