3ebb13e4aec5c92f0d66e7c58b5a7d5989af7b79
facundo <azulafacundo@hotmail.com>
2017-05-26 23:35:17 -0300

    merge comandosvoz

--- static/js/comandosdevoz.js
+++ static/js/comandosdevoz.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+responsiveVoice.setDefaultVoice("Spanish Latin American Female");
 //($('#gmailDiv').hide(); 
 //se crea una funcion
 function abrirNoticias() {
@@ -21,6 +23,32 @@ function abrirGmail() {
 function cerrarGmail() {
     //	 
     $('#gmailDiv').hide();
+    //
+}
+function pedirHora() {
+    //	 
+
+	responsiveVoice.speak( document.getElementById("hora").innerHTML);
+    //
+}
+function pedirTemperatura() {
+    //	 
+	
+	var temp=document.getElementById("temperatura").innerHTML;
+	temp=temp.substring(0,temp.length-2);
+	responsiveVoice.speak( temp + " grados");
+    //
+}
+function pedirFecha() {
+    //	 
+
+	responsiveVoice.speak( document.getElementById("fecha").innerHTML);
+    //
+}
+function pedirClima() {
+    //	 
+
+	responsiveVoice.speak( "está " + document.getElementById("clima").innerHTML);
     //
 }
 
@@ -47,6 +75,18 @@ if (annyang) {
         'cerrar gmail': function() {
             cerrarGmail();
         },
+		'qué hora es': function() {
+            pedirHora();
+        },
+		'temperatura': function() {
+            pedirTemperatura();
+        },
+		'fecha': function() {
+            pedirFecha();
+        },
+		'clima': function() {
+            pedirClima();
+        },
 
 
         //{ alert('Hello world!'); } //imprime un mensaje
@@ -61,3 +101,4 @@ if (annyang) {
     //comienza a escuchar
     annyang.start();
 }
+});
